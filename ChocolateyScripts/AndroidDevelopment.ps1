d2676bf7276b1b577a3237a3a31701cf22d14a49
Carlo Barraco <carlobarraco@gmail.com>
2020-05-09 18:40:31 -0400

    Added choco scripts

---
+++ ChocolateyScripts/AndroidDevelopment.ps1
@@ -0,0 +1,2 @@
+choco install androidstudio
+choco install flutter
---
+++ ChocolateyScripts/BasicDevelopment.ps1
@@ -0,0 +1,7 @@
+choco install git
+choco install github-desktop
+choco install putty
+choco install vscode
+choco install powershell-core
+choco install sysinternals
+choco install curl
